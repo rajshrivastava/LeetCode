e9e16e6410c9f0512bad4fe966e0b93ea9d651d5
raj <rkshriva@ncsu.edu>
2021-01-15 14:35:32 -0500

    Adding 54. Spiral Matrix.py

--- 54. Spiral Matrix.py
+++ 54. Spiral Matrix.py
@@ -1,28 +1,27 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        directions = [[0,1], [1,0],[0,-1],[-1,0]]
-        r = len(matrix)
-        if r==0:
-            return []
-        else:
-            c = len(matrix[0])
+        def offBound(x, y):
+            # if any((x < 0, x > m-1, y<0, y>n-1, matrix[x][y]==None)):
+            if x < 0 or x > m-1 or y<0 or y>n-1 or matrix[x][y]==None:
+                return True
+            return False
         
-        dirIdx = 0
-        i,j = 0,0
-        seen = [[False] * c for _ in matrix]
-        spiral = []
-        for _ in range(r*c):
-            spiral.append(matrix[i][j]) #append the element to the answer
-            seen[i][j] = True
-            
-            i_ = i + directions[dirIdx][0]
-            j_ = j + directions[dirIdx][1]
-            
-            if i_<0 or i_>r-1 or j_<0 or j_>c-1 or seen[i_][j_]: #collision
-                dirIdx = (dirIdx + 1)%4
-            
-            i += directions[dirIdx][0]
-            j += directions[dirIdx][1]
+        directions = [(0,1), (1,0), (0,-1),(-1,0)]
+        curr_dir = 0
+        result = [matrix[0][0]]
+        matrix[0][0]=None
+        m = len(matrix)
+        n = len(matrix[0])
+        x,y=0,0
+        for i in range(m*n-1):
+            dx, dy = directions[curr_dir]
+            if offBound(x+dx, y+dy):
+                curr_dir = (curr_dir+1)%4
+                dx, dy = directions[curr_dir]
+            x += dx
+            y += dy
+            result.append(matrix[x][y])
+            matrix[x][y] = None
+        return result
             
         
-        return spiral
