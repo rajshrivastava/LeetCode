2dc0c698c386247022043e5522957e56b321a71f
raj <rkshriva@ncsu.edu>
2021-01-16 15:32:26 -0500

    Adding 198. House Robber.py

---
+++ 198. House Robber.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums: return 0
+        n = len(nums)
+        dp = [0]*(n+1)  
+        dp[1] = nums[0]
+        for i in range(2, n+1):
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i-1])
+        return dp[-1]
+            
