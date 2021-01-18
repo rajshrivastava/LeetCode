09314ac8fd5ab35730a0d92e93bebd9b51fad59c
raj <rkshriva@ncsu.edu>
2021-01-17 22:50:30 -0500

    Adding 1004. Max Consecutive Ones III.py

--- 1004. Max Consecutive Ones III.py
+++ 1004. Max Consecutive Ones III.py
@@ -1,18 +1,18 @@
 class Solution:
     def longestOnes(self, arr: List[int], k: int) -> int:
-        count  = [0,0]
-        
-        maxCount = 0
+        left = 0
         longest = 0
-        window_start = 0
-        for window_end in range(len(arr)):
-            count[arr[window_end]] += 1
-            maxCount = max(maxCount, count[1])
+        n = len(arr)
+        for right in range(n):
+            if arr[right] == 0:
+                k-=1
+            
+            if k < 0:
+                while left < n and arr[left] != 0:
+                    left += 1
+                left += 1
+                k = 0
+            longest = max(longest, right-left+1)    
+            # print(left, right, arr[right], longest, k)
 
-            if window_end - window_start + 1 - maxCount > k:
-                count[arr[window_start]] -= 1
-                window_start += 1
-
-            longest = max(longest, window_end -window_start +1)
-
-        return longest
+        return longest        
