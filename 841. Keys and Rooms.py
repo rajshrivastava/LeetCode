714f7d25d8906dfd6deb742e4d3838cae0f1c588
rajshrivastava <rkshriva@ncsu.edu>
2021-02-21 13:33:49 -0500

    Adding 841. Keys and Rooms.py

--- 841. Keys and Rooms.py
+++ 841. Keys and Rooms.py
@@ -1,11 +1,17 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        queue = collections.deque([0])
+        #Can be done either using stack (DFS) or queue (BFS)
+        
+        # queue = collections.deque([0])
+        stack = [0]
         n = len(rooms)
         visited = {0}
         count = 0
-        while queue:
-            room = queue.popleft()
+        
+        # while queue:
+        while stack:
+            # room = queue.popleft()
+            room = stack.pop()
             count += 1
 
             if count == n:
@@ -13,7 +19,8 @@ class Solution:
             
             for nextRoom in rooms[room]:
                 if nextRoom not in visited:
-                    queue.append(nextRoom)
+                    # queue.append(nextRoom)
+                    stack.append(nextRoom)
                     visited.add(nextRoom)
         return False
             
