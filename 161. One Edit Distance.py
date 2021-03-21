74aad8ef83a9136b1bd57527c9cb1bc522d9a0c5
rajshrivastava <rkshriva@ncsu.edu>
2021-03-21 14:08:14 -0400

    161. One Edit Distance.py

--- 161. One Edit Distance.py
+++ 161. One Edit Distance.py
@@ -1,28 +1,22 @@
 class Solution:
     def isOneEditDistance(self, s: str, t: str) -> bool:
-        n1, n2 = len(s), len(t)
-        if n1 < n2:
-            n1, n2 = n2, n1
-            s, t = t, s
-            
-        if n1 - n2 > 1:
+        '''
+        1. Traverse from left and right. Stop with first non-matching
+        '''
+        s_len, t_len = len(s), len(t)
+        if abs(s_len - t_len) > 1:
             return False
         
-        i = 0
-        while i < n2 and s[i] == t[i]:
-            i += 1
+        # traversing from left
+        n = min(s_len, t_len)
+        left = 0
+        while left < n and s[left] == t[left]:
+            left += 1
         
-        if i > n2:
-            return n1 != n2
-        
-        i += 1
-        if n1 == n2:
-            j = i
+        # if the strings are same
+        if s_len == t_len == left:
+            return False
+        elif s_len == t_len:
+            return s[left+1:] == t[left+1:]
         else:
-            j = i-1
-       
-        while i < n1 and s[i] == t[j]:
-            i += 1
-            j += 1
-        
-        return i == n1
+            return s[left+1:] == t[left:] if s_len > t_len else s[left:] == t[left+1:]
